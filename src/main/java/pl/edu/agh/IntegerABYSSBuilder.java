fdf424aef7949c50e27c58dba589df350eb87f34
Radoslaw Lesniewski <Radoslaw.Lesniewski@sabre.com>
2017-12-11 22:12:14 +0100

    Refactoring

--- src/main/java/pl/edu/agh/IntegerABYSSBuilder.java
+++ src/main/java/pl/edu/agh/IntegerABYSSBuilder.java
@@ -1,30 +1,24 @@
 package pl.edu.agh;
 
-import org.uma.jmetal.algorithm.multiobjective.abyss.ABYSS;
 import org.uma.jmetal.operator.CrossoverOperator;
 import org.uma.jmetal.operator.LocalSearchOperator;
 import org.uma.jmetal.operator.MutationOperator;
 import org.uma.jmetal.operator.impl.crossover.PMXCrossover;
-import org.uma.jmetal.operator.impl.crossover.TwoPointCrossover;
 import org.uma.jmetal.operator.impl.localsearch.ArchiveMutationLocalSearch;
-import org.uma.jmetal.operator.impl.mutation.IntegerPolynomialMutation;
 import org.uma.jmetal.operator.impl.mutation.PermutationSwapMutation;
-import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
 import org.uma.jmetal.problem.impl.AbstractIntegerPermutationProblem;
-import org.uma.jmetal.solution.DoubleSolution;
 import org.uma.jmetal.solution.PermutationSolution;
 import org.uma.jmetal.util.AlgorithmBuilder;
 import org.uma.jmetal.util.archive.Archive;
 import org.uma.jmetal.util.archive.impl.CrowdingDistanceArchive;
 
 /**
- *  @author Cristobal Barba
+ * @author Cristobal Barba
  */
 public class IntegerABYSSBuilder implements AlgorithmBuilder<IntegerABYSS> {
-    private AbstractIntegerPermutationProblem problem  ;
-    private CrossoverOperator crossoverOperator   ;
-    protected LocalSearchOperator<PermutationSolution<Integer>> improvementOperator ;
-
+    protected LocalSearchOperator<PermutationSolution<Integer>> improvementOperator;
+    private AbstractIntegerPermutationProblem problem;
+    private CrossoverOperator crossoverOperator;
     private MutationOperator<PermutationSolution<Integer>> mutationOperator;
     private int numberOfSubranges;
     private int populationSize;
@@ -34,27 +28,26 @@ public class IntegerABYSSBuilder implements AlgorithmBuilder<IntegerABYSS> {
     private int maxEvaluations;
     private CrowdingDistanceArchive<PermutationSolution<Integer>> archive;
 
-    public IntegerABYSSBuilder(AbstractIntegerPermutationProblem problem, Archive<PermutationSolution<Integer>> archive){
+    public IntegerABYSSBuilder(AbstractIntegerPermutationProblem problem, Archive<PermutationSolution<Integer>> archive) {
         this.populationSize = 20;
-        this.maxEvaluations = 40;
+        this.maxEvaluations = 60;
         this.archiveSize = 100;
         this.refSet1Size = 10;
         this.refSet2Size = 10;
         this.numberOfSubranges = 4;
         this.problem = problem;
         double crossoverProbability = 0.9;
-        double distributionIndex=20.0;
         this.crossoverOperator = new PMXCrossover(crossoverProbability);
-        double mutationProbability= 1.0/problem.getNumberOfVariables();
+        double mutationProbability = 0.5;
         this.mutationOperator = new PermutationSwapMutation<>(mutationProbability);
-        int improvementRounds= 1;
-        this.archive =(CrowdingDistanceArchive<PermutationSolution<Integer>>)archive;
-        this.improvementOperator = new ArchiveMutationLocalSearch<PermutationSolution<Integer>>(improvementRounds,mutationOperator,this.archive,problem);
+        int improvementRounds = 1;
+        this.archive = (CrowdingDistanceArchive<PermutationSolution<Integer>>) archive;
+        this.improvementOperator = new ArchiveMutationLocalSearch<PermutationSolution<Integer>>(improvementRounds, mutationOperator, this.archive, problem);
     }
 
     @Override
     public IntegerABYSS build() {
-        return new IntegerABYSS(problem, maxEvaluations, populationSize,refSet1Size,refSet2Size,archiveSize,
+        return new IntegerABYSS(problem, maxEvaluations, populationSize, refSet1Size, refSet2Size, archiveSize,
                 archive, improvementOperator, crossoverOperator, numberOfSubranges);
     }
 }
--- src/main/java/pl/edu/agh/Main.java
+++ src/main/java/pl/edu/agh/Main.java
@@ -1,32 +1,13 @@
 package pl.edu.agh;
 
-import org.uma.jmetal.algorithm.impl.AbstractEvolutionaryAlgorithm;
-import org.uma.jmetal.algorithm.impl.ScatterSearch;
-import org.uma.jmetal.algorithm.multiobjective.abyss.ABYSS;
-import org.uma.jmetal.algorithm.multiobjective.abyss.ABYSSBuilder;
-import org.uma.jmetal.algorithm.singleobjective.geneticalgorithm.GeneticAlgorithmBuilder;
-import org.uma.jmetal.operator.CrossoverOperator;
-import org.uma.jmetal.operator.MutationOperator;
-import org.uma.jmetal.operator.SelectionOperator;
-import org.uma.jmetal.operator.impl.crossover.DifferentialEvolutionCrossover;
-import org.uma.jmetal.operator.impl.crossover.PMXCrossover;
-import org.uma.jmetal.operator.impl.mutation.PermutationSwapMutation;
-import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
-import org.uma.jmetal.operator.impl.selection.BinaryTournamentSelection;
-import org.uma.jmetal.problem.DoubleProblem;
-import org.uma.jmetal.problem.PermutationProblem;
 import org.uma.jmetal.problem.singleobjective.TSP;
-import org.uma.jmetal.solution.DoubleSolution;
 import org.uma.jmetal.solution.PermutationSolution;
 import org.uma.jmetal.util.AlgorithmRunner;
 import org.uma.jmetal.util.JMetalLogger;
 import org.uma.jmetal.util.archive.impl.CrowdingDistanceArchive;
-import org.uma.jmetal.util.archive.impl.NonDominatedSolutionListArchive;
-import org.uma.jmetal.util.comparator.RankingAndCrowdingDistanceComparator;
 import org.uma.jmetal.util.fileoutput.SolutionListOutput;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -34,34 +15,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         TSP problem;
         IntegerABYSS algorithm;
-        CrossoverOperator<DoubleSolution> crossover;
-        MutationOperator<DoubleSolution> mutation;
-//        SelectionOperator<List<PermutationSolution<Integer>>, PermutationSolution<Integer>> selection;
-
         JMetalLogger.logger.info("TSP");
 
         problem = new TSP("/tsp/kroA100.tsp");
 
-        crossover = new DifferentialEvolutionCrossover() ;
-        mutation = new PolynomialMutation();
-
-//        algorithm = new ABYSSBuilder(problem, null)
-//                .setPopulationSize(100)
-//                .setMaxEvaluations(100000)
-//                .setCrossoverOperator(crossover)
-//                .setMutationOperator(mutation)
-//                //TODO here inject local search for improvement
-////                .setImprovementOperator()
-//                .build();
-
         algorithm = new IntegerABYSSBuilder(problem, new CrowdingDistanceArchive<>(10)).build();
 
         AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
-                .execute() ;
+                .execute();
 
-        List<PermutationSolution<Integer>> solution = algorithm.getResult() ;
+        List<PermutationSolution<Integer>> solution = algorithm.getResult();
 
-        long computingTime = algorithmRunner.getComputingTime() ;
+        long computingTime = algorithmRunner.getComputingTime();
 
         new SolutionListOutput(solution)
                 .print();
